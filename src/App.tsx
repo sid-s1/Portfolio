51cf1ed7f3c90717ca47e5ef7e3142e296165c97
Siddharth Singh <siddharth.s1@outlook.com>
2024-01-30 07:27:46 +1100

    Adding more basic elements to the skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import github from './github.png';
+import linkedin from './linkedin.png';
+import email from './email.png';
 
 const App: React.FC = () => {
   return (
@@ -9,6 +12,18 @@ const App: React.FC = () => {
         <div className="header-icons">Projects</div>
         <div className="header-icons">About</div>
       </header>
+      <div className="main-block">
+        <div className="content">
+          <h1 className='name'>SIDDHARTH SINGH</h1>
+          <h3 className='title'>SOFTWARE ENGINEER</h3>
+          <div className="socials-links">
+            <img src={github} alt="github-logo" className="link-icons"/>
+            <img src={linkedin} alt="linkedin-logo" className="link-icons"/>
+            <img src={email} alt="email-logo" className="link-icons"/>
+          </div>
+        </div>
+        <div className="profile-photo"></div>
+      </div>
     </div>
   );
 }
