2a4310353b0193ce384ee51b0b7c599bdd93fbbd
Siddharth Singh <siddharth.s1@outlook.com>
2024-01-30 06:48:47 +1100

    Setting up skeleton

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="landing-header">
+        <div className="header-icons">Home</div>
+        <div className="header-icons">Projects</div>
+        <div className="header-icons">About</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
