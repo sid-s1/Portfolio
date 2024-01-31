1e4016e2ceb4fa958322382b3f086cc78c071811
Siddharth Singh <siddharth.s1@outlook.com>
2024-02-01 08:04:21 +1100

    finishing the home page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import github from './github.png';
 import linkedin from './linkedin.png';
 import email from './email.png';
+import profile from './profile2.jpg';
 
 const App: React.FC = () => {
   return (
@@ -14,15 +15,20 @@ const App: React.FC = () => {
       </header>
       <div className="main-block">
         <div className="content">
-          <h1 className='name'>SIDDHARTH SINGH</h1>
-          <h3 className='title'>SOFTWARE ENGINEER</h3>
+          <p className='name'>SIDDHARTH SINGH</p>
+          <p className='title'>SOFTWARE ENGINEER</p>
+          <div className='profile-photo-block-mobile'>
+            <img src={profile} alt='profile-photo'className='profile-photo'/>
+          </div>
           <div className="socials-links">
             <img src={github} alt="github-logo" className="link-icons"/>
             <img src={linkedin} alt="linkedin-logo" className="link-icons"/>
             <img src={email} alt="email-logo" className="link-icons"/>
           </div>
         </div>
-        <div className="profile-photo"></div>
+        <div className='profile-photo-block'>
+          <img src={profile} alt='profile-photo'className='profile-photo'/>
+        </div>
       </div>
     </div>
   );
