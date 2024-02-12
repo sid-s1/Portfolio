e8c5dc169692d915fc66b272f3d6f9bf64c6ee90
Siddharth Singh <siddharth.s1@outlook.com>
2024-02-13 07:26:47 +1100

    changes to homepage design

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Header from "./Header";
-import github from '../github.png';
-import linkedin from '../linkedin.png';
+// import github from '../github.png';
+// import linkedin from '../linkedin.png';
+import github from '../github final.jpg';
+import linkedin from '../linkedin.svg';
 import email from '../email.png';
 import profile from '../profile2.jpg';
 
@@ -16,19 +18,22 @@ const Homepage: React.FC<HomepageProps> = ({changeMainPage}) => {
         <Header onNavbarClick={onNavbarClick}/>
         <div className="main-block">
             <div className="content">
-            <p className='name'>SIDDHARTH SINGH</p>
-            <p className='title'>SOFTWARE ENGINEER</p>
+            <div>
+                <p className='name'>SIDDHARTH SINGH</p>
+                <p className='title'>SOFTWARE ENGINEER</p>
+            </div>
             <div className='profile-photo-block-mobile'>
                 <img src={profile} alt='profile-photo'className='profile-photo'/>
+                <div className="contact-me">Contact Me</div>
             </div>
             <div className="socials-links">
-                <img src={github} alt="github-logo" className="link-icons"/>
-                <img src={linkedin} alt="linkedin-logo" className="link-icons"/>
-                <img src={email} alt="email-logo" className="link-icons"/>
+                <img src={github} alt="github-logo" className="link-icons github"/>
+                <img src={linkedin} alt="linkedin-logo" className="link-icons linkedin"/>
             </div>
             </div>
             <div className='profile-photo-block'>
-            <img src={profile} alt='profile-photo'className='profile-photo'/>
+                <img src={profile} alt='profile-photo'className='profile-photo'/>
+                <ul className="contact-me"><li>Contact Me</li></ul>
             </div>
         </div>
       </>
