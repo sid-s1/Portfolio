1052a659491cebc11fa797eea690024e90f07113
Siddharth Singh <siddharth.s1@outlook.com>
2024-02-17 10:10:08 +1100

    ensuring mobile view contact us button also clickable

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -30,7 +30,7 @@ const Homepage: React.FC<HomepageProps> = ({changeMainPage}) => {
                 </div>
                 <div className='profile-photo-block-mobile'>
                     <img src={profile} alt='profile-photo'className='profile-photo'/>
-                    <div className="contact-me">Contact Me</div>
+                    <div className="contact-me" onClick={contactMeAction}>Contact Me</div>
                 </div>
                 <div className="socials-links">
                     <a href="https://github.com/sid-s1?tab=repositories" target="blank">
