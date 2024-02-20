c99b0dae2fed7ced5a7fbc926f43db39d6e58ff4
Siddharth Singh <siddharth.s1@outlook.com>
2024-02-20 21:53:26 +1100

    changes to tsconfig to allow importing pdf to display resume on website

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Resume from "../documents/Siddharth Singh - Software Engineer.pdf";
 
 interface AboutProps {
     changeMainPage: (param: string) => void;
@@ -11,6 +12,7 @@ const About: React.FC<AboutProps> = ({changeMainPage}) => {
     <>
         <Header onNavbarClick={onNavbarClick}/>
         <h1>ABOUT</h1>
+        <a href={Resume} target="_blank">Download my resume</a>
     </>
     )
 };
